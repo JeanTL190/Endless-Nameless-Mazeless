ce10501b69808db49782617edb5a90b85e3ea36a
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-10-02 23:07:31 -0300

    PLayer movendo e camera seguindo com delay

---
+++ Endless-Nameless-Mazeless/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector2 maxXandY;
+    [SerializeField] private Vector2 minXandY;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    private Transform player;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - player.position;
+    }
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            /*float x = Mathf.Clamp(player.position.x, minXandY.x, maxXandY.x);
+            float y = Mathf.Clamp(player.position.y, minXandY.y, maxXandY.y);
+            this.transform.position = new Vector3(x, y, this.transform.position.z);*/
+            Vector3 desiredPosition = player.position + offset;
+            Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            transform.position = SmoothedPosition;
+        }
+    }
+}
